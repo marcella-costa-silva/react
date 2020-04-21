c4db9b23c374d024d1e3c1382480b10314721cc9
Marcella <marcellatsangos@gmail.com>
2020-04-21 11:14:57 -0300

    Product list

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import api from '../../services/api'
 
+import './styles.css'
+
 export default class Main extends Component {
   state = {
     products: []
@@ -18,11 +20,18 @@ export default class Main extends Component {
   }
 
   render() {
+    const { products } = this.state
+
     // return <h1>Quantidade de Produtos: {this.state.products.length}</h1>
     return (
       <div className='product-list'>
-        {this.state.products.map(product => (
-          <h2 key='product._id'>{product.title}</h2>
+        {products.map(product => (
+          <article key='products._id'>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <a href='#'>Acessar</a>
+          </article>
         ))}
       </div>
     )
