37f970965a6ff708e2a7e0e0f30a969bbe675c02
Marcella <marcellatsangos@gmail.com>
2020-04-21 12:52:36 -0300

    Previous/Next page

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -5,7 +5,9 @@ import './styles.css'
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1
   }
 
   // Executado assim que o componente for executado em tela.
@@ -13,14 +15,29 @@ export default class Main extends Component {
     this.loadProducts()
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products')
-    console.log(response.data.docs)
-    this.setState({ products: response.data.docs })
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`)
+    // console.log(response.data.docs)
+    const { docs, ...productInfo } = response.data
+    this.setState({ products: docs, productInfo, page })
+  }
+
+  prevPage = () => {
+    const { page, pageInfo } = this.state
+    if (page === 1) return
+    const pageNumber = page - 1
+    this.loadProducts(pageNumber)
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state
+    if (page === productInfo.pages) return
+    const pageNumber = page + 1
+    this.loadProducts(pageNumber)
   }
 
   render() {
-    const { products } = this.state
+    const { products, page, productInfo } = this.state
 
     // return <h1>Quantidade de Produtos: {this.state.products.length}</h1>
     return (
@@ -33,6 +50,11 @@ export default class Main extends Component {
             <a href='#'>Acessar</a>
           </article>
         ))}
+
+        <div className='actions'>
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+        </div>
       </div>
     )
   }
