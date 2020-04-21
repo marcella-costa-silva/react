1442673c4e526d5303055e72cc20c03d16ea9641
Marcella <marcellatsangos@gmail.com>
2020-04-21 09:42:23 -0300

    Header creation

---
+++ huntweb/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css'
+import Header from './components/Header'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//     </div>
+//   );
+// }
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+)
+
+export default App;
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+// class Header extends Component {
+//   render() {
+//     return <h1>Hello</h1>
+//   }
+// }
+
+const Header = () => <header id="main-header">Hello People</header>
+
+export default Header
